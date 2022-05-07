5d381013aae6e8c0b87a66731955e6639a7d2bdf
Егор Ткаченко <egortkachenco@gmail.com>
2022-05-07 10:29:19 +0300

    deploy script fix, env fix

--- env-config.js
+++ env-config.js
@@ -1,4 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
+console.log('PRODUCTION', process.env.NODE_ENV)
 module.exports = {
   'process.env.BACKEND_URL': prod ? '/smart-night-stand-landing' : '',
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const debug = process.env.NODE_ENV !== 'production'
+console.log('DEBUG: ', debug)
 const nextConfig = {
+  env: {
+    BACKEND_URL: !debug
+      ? 'https://egotkachenco.github.io/smart-night-stand-landing'
+      : '',
+  },
   reactStrictMode: true,
   exportPathMap: function () {
     return {
